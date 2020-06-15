5e87f019619aa2865fa1b27f063c2eed4f7faffe
Grigory Shipunov <blame@oxapentane.com>
2020-06-15 19:24:36 +0200

    keep it dark in terminals
    most GUIs are light, so use light theme there
    most terminals are dark by default, so use dark theme here

--- init.el
+++ init.el
@@ -138,7 +138,9 @@
 (straight-use-package 'leuven-theme)
 (straight-use-package 'zenburn-theme)
 
-(load-theme 'leuven t)
+(if (display-graphic-p)
+    (load-theme 'leuven t)
+  (load-theme 'zenburn t))
 
 ;; CC mode default styles
 (setq c-default-style '((java-mode . "java")
